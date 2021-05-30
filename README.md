e9635c7711afae716623569ea28613ee9e6e9dbe
Carlos Cazelatto Junior <carlosjunior1983@gmail.com>
2021-05-30 18:53:19 -0300

    Project created

---
+++ README.md
@@ -0,0 +1,22 @@
+# CRUD Completo com Spring Boot REST API
+
+Estudos CRUD Completo com Spring Boot REST API
+
+
+## Projeto
+
+- BackEnd
+
+
+## Tecnologia
+
+`Java` `Spring Boot` 
+
+  
+
+
+
+
+
+
+Créditos: JDev/SpringBoot
---
+++ backend/src/test/java/br/com/carlosjunior/cursojdev/CursoJdevApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.carlosjunior.cursojdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoJdevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
