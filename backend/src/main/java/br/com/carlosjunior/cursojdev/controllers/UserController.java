ce0ad1927301ad612bc96989c4a71f3e68809ca1
Carlos Cazelatto Junior <carlosjunior1983@gmail.com>
2021-05-31 22:34:32 -0300

    Method List created

--- backend/src/main/java/br/com/carlosjunior/cursojdev/controllers/UserController.java
+++ backend/src/main/java/br/com/carlosjunior/cursojdev/controllers/UserController.java
@@ -1,6 +1,10 @@
 package br.com.carlosjunior.cursojdev.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,16 +19,22 @@ public class UserController {
 
 	@Autowired
 	private UserRepository userRepository;
-	
-	@GetMapping("/{name}")
+
+	@GetMapping(value = "/{name}")
 	public String test(@PathVariable String name) {
-		
+
 		User usuario = new User();
 		usuario.setName(name);
 		usuario.setAge(37);
 		userRepository.save(usuario);
-		
+
 		return "Salvo com sucesso!";
 	}
-	
+
+	@GetMapping(value = "/list")
+	public ResponseEntity<List<User>> listAll() {
+		List<User> users = userRepository.findAll();
+		return new ResponseEntity<List<User>>(users, HttpStatus.OK);
+	}
+
 }
--- backend/src/main/java/br/com/carlosjunior/cursojdev/services/UserService.java
+++ backend/src/main/java/br/com/carlosjunior/cursojdev/services/UserService.java
@@ -1,13 +1,10 @@
 package br.com.carlosjunior.cursojdev.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import br.com.carlosjunior.cursojdev.repositories.UserRepository;
 
 @Service
 public class UserService {
 
-
+	
 	
 }
