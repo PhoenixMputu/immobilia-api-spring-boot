0ff4041d4e96a973861388cd96ecb57064388b14
Carlos Cazelatto Junior <carlosjunior1983@gmail.com>
2021-05-31 23:23:08 -0300

    Method delete created

--- backend/src/main/java/br/com/carlosjunior/cursojdev/controllers/UserController.java
+++ backend/src/main/java/br/com/carlosjunior/cursojdev/controllers/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -51,6 +52,18 @@ public class UserController {
 		return new ResponseEntity<List<User>>(users, HttpStatus.OK);
 	}
 	
+	@DeleteMapping(value = "/delete")
+	public ResponseEntity<User> delete(@RequestBody User user) {
+		userRepository.deleteById(user.getId());
+		return new ResponseEntity<User>(user, HttpStatus.OK);
+	}	
+	
+	@DeleteMapping(value = "/delete/{id}")
+	public ResponseEntity<String> delete(@PathVariable Long id) {
+		userRepository.deleteById(id);
+		return new ResponseEntity<String>("ID: "+id+" deleted success", HttpStatus.OK);
+	}	
+		
 	
 
 }
